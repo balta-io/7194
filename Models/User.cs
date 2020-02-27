ae342d9949863c8e72d3ee94c1a0a03af2a9e6a4
Gedan Magalhães <gedanycar@gmail.com>
2020-02-26 23:18:45 -0300

    Update User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -8,13 +8,13 @@ namespace Backoffice.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Este campo é obrigatório")]
-        [MaxLength(20, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
-        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        [MaxLength(20, ErrorMessage = "Este campo deve conter entre 3 e 20 caracteres")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 20 caracteres")]
         public string Username { get; set; }
 
         [Required(ErrorMessage = "Este campo é obrigatório")]
-        [MaxLength(20, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
-        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        [MaxLength(20, ErrorMessage = "Este campo deve conter entre 3 e 20 caracteres")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 20 caracteres")]
         public string Password { get; set; }
 
         public string Role { get; set; }
